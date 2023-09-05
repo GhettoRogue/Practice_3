82ffd56d3c9132ed99e5252b4eaf8c81e962e433
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-09-05 18:24:48 +0400

    Initial commit

---
+++ Practice_3/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace Practice_3;
+
+public class Employee
+{
+    public int Id { get; init; }
+    public string FullName { get; init; } 
+    public string Department { get; set; } 
+    public string Position { get; set; } 
+}
---
+++ Practice_3/Program.cs
@@ -0,0 +1,45 @@
+﻿using Practice_3;
+
+var employees = new List<Employee>()
+{
+    new Employee() { Id = 1, FullName = "Vladimir Ignatiew", Position = "Engineer", Department = "Development" },
+    new Employee() { Id = 2, FullName = "Johny Smith", Position = "Manager", Department = "Sales" },
+    new Employee() { Id = 3, FullName = "John Smith", Position = "Accountant", Department = "Financial" }
+};
+
+var search = UserInput();
+FindEmployees(search, employees);
+
+return;
+
+string UserInput()
+{
+    Console.Write("Enter ID, full name, position or department of employee: ");
+    var str = Console.ReadLine()!;
+    return str;
+}
+
+static void FindEmployees(string search, List<Employee> employees)
+{
+    var result = employees.FindAll(
+        temp =>
+            temp.Id.ToString() == search ||
+            temp.FullName.ToLower().Contains(search.ToLower()) ||
+            temp.Position.ToString() == search ||
+            temp.Department.ToString() == search);
+
+    if (result.Count > 0)
+    {
+        Console.WriteLine($"Found {result.Count} employee.");
+        foreach (var employee in result)
+        {
+            Console.WriteLine(
+                $"ID: {employee.Id}," +
+                $" FullName: {employee.FullName}," +
+                $" Position: {employee.Position}," +
+                $" Department: {employee.Department}");
+        }
+    }
+
+    Console.WriteLine("Employees not found.");
+}
